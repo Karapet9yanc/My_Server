4e471e0f4f1b50bdbca5712798d9d50bd837f666
Vladislav Karapet9yanc <vl@gmail.com>
2021-11-18 13:07:57 +0300

    Code Review

---
+++ src/modules/controllers/task.controller.js
@@ -0,0 +1,47 @@
+const Task = require("../../db/models/task/index");
+
+module.exports.getAllTasks = (req, res) => {
+  Task.find().then((result) => {
+    res.send({ data: result });
+  });
+};
+
+module.exports.createNewTask = (req, res) => {
+  if (req.body.hasOwnProperty("text") && req.body.hasOwnProperty("isCheck")) {
+    const task = new Task(req.body);
+    task.save().then((result) => {
+      Task.find().then((result) => {
+        res.send({ data: result });
+      });
+    });
+  } else {
+    res.status(500).send("Error");
+  }
+};
+
+module.exports.changeTaskInfo = (req, res) => {
+  if (
+    req.body.hasOwnProperty("_id") && 
+    (req.body.hasOwnProperty("text") || req.body.hasOwnProperty("isCheck"))
+  ) {
+    Task.updateOne({ _id: req.body._id }, req.body).then((result) => {
+      Task.find().then((result) => {
+        res.send({ data: result });
+        });
+    });
+  } else {
+    res.status(500).send("Error");
+  }
+};
+
+module.exports.deleteTask = (req, res) => {
+  if (req.query._id) {
+    Task.deleteOne({ _id: req.query._id }).then((result) => {
+      Task.find().then((result) => {
+        res.send({ data: result });
+      });
+    });
+  } else {
+    res.status(500).send("Error");
+  }
+};
